b2e557aba80bedd58345a2070e76d735b0af6050
Joshua Westerheide <dev@jdoubleu.de>
2018-05-21 13:29:04 +0200

    Add import assistant

--- src/gui/assistant/ImportAssistant.cpp
+++ src/gui/assistant/ImportAssistant.cpp
@@ -1,1 +1,12 @@
 #include "ImportAssistant.h"
+#include "ui_importassistant.h"
+
+using namespace gui::assistant;
+
+ImportAssistant::ImportAssistant(QWidget *parent): QDialog(parent), ui(new Ui::ImportAssistant) {
+    ui->setupUi(this);
+}
+
+ImportAssistant::~ImportAssistant() {
+    delete ui;
+}
--- src/gui/assistant/ImportAssistant.h
+++ src/gui/assistant/ImportAssistant.h
@@ -1,9 +1,20 @@
-#include "src/gui/assistant/Assistant.h"
+#include <QDialog>
+
+namespace Ui {
+class ImportAssistant;
+}
 
 namespace gui::assistant {
 
-class ImportAssistant : public Assistant {
+class ImportAssistant: public QDialog {
+    Q_OBJECT
 
+public:
+    explicit ImportAssistant(QWidget *parent = 0);
+    ~ImportAssistant();
+
+private:
+    Ui::ImportAssistant *ui;
 };
 
 }
