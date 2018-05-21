e7a1a8adb040635680056689166494523a3d1876
Joshua Westerheide <dev@jdoubleu.de>
2018-05-21 11:43:38 +0200

    Fix cert namespace

--- src/cert/Certificate.h
+++ src/cert/Certificate.h
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-namespace {
+namespace cert {
 
 class Certificate {
 public:
--- src/cert/CertificateList.h
+++ src/cert/CertificateList.h
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-namespace {
+namespace cert {
 
 class CertificateList {
 protected:
