365b810244d03e9fd24a68d14dd494b7cdb18f73
Joshua Westerheide <dev@jdoubleu.de>
2018-05-25 17:39:43 +0200

    Implement X509_NAME convert to map util function

--- src/cert/Util.h
+++ src/cert/Util.h
@@ -1,12 +1,16 @@
 #include <openssl/ossl_typ.h>
 #include <string>
 #include <ctime>
+#include <map>
 
 using std::string;
+using std::map;
 
 namespace cert {
 
     string X509_NAME_to_combined_string(X509_NAME *name);
+
+    map<string, string> X509_NAME_to_map(const X509_NAME *entries);
 
     time_t ASN1_TIME_to_time(ASN1_TIME *time);
 
