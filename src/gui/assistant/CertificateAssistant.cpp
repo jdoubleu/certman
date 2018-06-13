da4c4ec944cab9a6ed5e700df4794bcd77ef533b
Joshua Westerheide <dev@jdoubleu.de>
2018-06-13 12:50:56 +0200

    Fill countryName_field with ISO 3166 alpha2 codes

--- src/gui/assistant/CertificateAssistant.cpp
+++ src/gui/assistant/CertificateAssistant.cpp
@@ -1,11 +1,25 @@
 #include "CertificateAssistant.h"
 #include "ui_certificateassistant.h"
+#include "../../core/Environment.h"
+#include <string>
+#include <QStringList>
+#include <iostream>
+
+using std::string;
+using std::cout;
+using core::Environment;
 
 using namespace gui::assistant;
 
 CertificateAssistant::CertificateAssistant(QWidget *parent) : QWizard(parent),
                                                               ui(new Ui::CertificateAssistant) {
     ui->setupUi(this);
+
+    // Fill country name field with iso3166 alpha2 codes
+    for (string cc : Environment::iso3166_alpha2_codes_list) {
+        ui->countryName_field->addItem(QString::fromStdString(cc));
+    }
+    ui->countryName_field->setCurrentText(QLocale::system().name().remove(0, 3));
 }
 
 CertificateAssistant::~CertificateAssistant() {
