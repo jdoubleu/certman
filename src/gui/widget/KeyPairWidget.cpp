f478e8020bdeae99c625a4d549ad457b944f8b21
Joshua Westerheide <dev@jdoubleu.de>
2018-06-30 19:57:47 +0200

    Implement base functionality of KeyPairWidget

--- src/gui/widget/KeyPairWidget.cpp
+++ src/gui/widget/KeyPairWidget.cpp
@@ -1,12 +1,72 @@
+#include <openssl/evp.h>
 #include "KeyPairWidget.h"
 #include "ui_keypairwidget.h"
+#include "PasswordWidget.h"
 
 using namespace gui::widget;
 
+const SUPPORTED_KEY_ALG supportedKeyAlgorithms[] = {
+    {"RSA", EVP_PKEY_RSA},
+    {"DSA", EVP_PKEY_DSA},
+};
+
+const SUPPORTED_WRAPPING_ALG supportedWrappingAlgorithms[] = {
+    {"DES", NULL},
+    {"DESede3 CBC", EVP_des_ede3_cbc()},
+    {"AES", NULL},
+    {"AES128 CBC", EVP_aes_128_cbc()},
+    {"AES256 CBC", EVP_aes_256_cbc()},
+    {"AES256 XTS", EVP_aes_256_xts()},
+    {"AES256 CFB128", EVP_aes_256_xts()}
+};
+
 KeyPairWidget::KeyPairWidget(QWidget *parent): QWidget(parent), ui(new Ui::KeyPairWidget) {
     ui->setupUi(this);
+
+    addSupportedKeyAlgorithms();
+    addSupportedKeyWrappingAlgorithms();
+
+    ui->keyPassword->setHidden(true);
 }
 
 KeyPairWidget::~KeyPairWidget() {
     delete ui;
+}
+
+void KeyPairWidget::addSupportedKeyAlgorithms() {
+    for (const SUPPORTED_KEY_ALG &alg : supportedKeyAlgorithms) {
+        ui->algorithmComboBox->addItem(tr(alg.name), QVariant(alg.algorithm));
+    }
+}
+
+void KeyPairWidget::addSupportedKeyWrappingAlgorithms() {
+    int row = 1;
+
+    for (const SUPPORTED_WRAPPING_ALG &wrappingAlg : supportedWrappingAlgorithms) {
+        QVariant data;
+        if (wrappingAlg.cipher != NULL) {
+            data = QVariant::fromValue(wrappingAlg);
+        }
+
+        ui->wrappingAlgorithmComboBox->addItem(tr(wrappingAlg.name), data);
+
+        if (wrappingAlg.cipher == NULL) {
+            ui->wrappingAlgorithmComboBox->setItemData(row, QVariant(0), Qt::UserRole - 1);
+        }
+
+        row++;
+    }
+}
+
+void KeyPairWidget::on_wrappingAlgorithmComboBox_currentIndexChanged(int index) {
+    // TODO: fix layout jumping after change
+    ui->keyPassword->setHidden(index == 0);
+}
+
+bool KeyPairWidget::validate() {
+    if (!ui->keyPassword->isHidden()) {
+
+    }
+
+    return true;
 }
--- src/gui/widget/KeyPairWidget.h
+++ src/gui/widget/KeyPairWidget.h
@@ -2,12 +2,27 @@
 #define KEYPAIRWIDGET_H
 
 #include <QWidget>
+#include <openssl/evp.h>
 
 namespace Ui {
     class KeyPairWidget;
 }
 
 namespace gui::widget {
+
+    struct algorithmComboBox_field_st {
+        const char *name;
+        const int algorithm;
+    };
+
+    typedef struct algorithmComboBox_field_st SUPPORTED_KEY_ALG;
+
+    struct wrappingAlgorithmComboBox_field_st {
+        const char *name;
+        const EVP_CIPHER *cipher;
+    };
+
+    typedef struct wrappingAlgorithmComboBox_field_st SUPPORTED_WRAPPING_ALG;
 
     class KeyPairWidget : public QWidget {
         Q_OBJECT
@@ -17,11 +32,23 @@ namespace gui::widget {
 
         ~KeyPairWidget() override;
 
+        bool validate();
+
+    public Q_SLOTS:
+
+        void on_wrappingAlgorithmComboBox_currentIndexChanged(int index);
+
     private:
 
         Ui::KeyPairWidget *ui;
+
+        void addSupportedKeyAlgorithms();
+
+        void addSupportedKeyWrappingAlgorithms();
     };
+
 };
 
+Q_DECLARE_METATYPE(gui::widget::SUPPORTED_WRAPPING_ALG);
 
 #endif //KEYPAIRWIDGET_H
