0d16a28d345981013b053f93c3de6264ab907670
Timo R <timo@ribbers.me>
2018-07-08 21:23:24 +0200

    Add documentation to certificate extension

--- src/cert/CertificateExtension.h
+++ src/cert/CertificateExtension.h
@@ -10,24 +10,56 @@ using std::map;
 
 namespace cert {
 
+    /**
+     * Represents a X509_EXTENSION.
+     */
     class CertificateExtension {
     public:
+        /**
+         * Constructor for a extension.
+         * @param extension X509_EXTENSION Pointer to the X509_EXTENSION.
+         */
         explicit CertificateExtension(X509_EXTENSION *extension);
 
+        /**
+         * Destructor for the extension.
+         */
         ~CertificateExtension();
 
+        /**
+         * Checks if the extension is critical.
+         * @return Get critical status.
+         */
         bool critical();
 
+        /**
+         * Get type in NID format.
+         * @return NID.
+         */
         int typeNID();
 
+        /**
+         * Get type of the extension.
+         * @return Type.
+         */
         string type();
 
+        /**
+         * Prints the extension.
+         * @return Extension formatted as a String.
+         */
         string sprint();
 
     private:
-
+        /**
+         * Pointer to the X509_EXTENSION structure.
+         */
         X509_EXTENSION *extension;
 
+        /**
+         * Get the extension structure.
+         * @return Pointer to a internal structure.
+         */
         void *value();
     };
 
