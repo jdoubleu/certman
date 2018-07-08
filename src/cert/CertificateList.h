f46cc5a7441af658cdae48a18ac3291cd3d88cbf
Max van Aerssen <code@maxva.de>
2018-07-08 22:51:50 +0200

    Add documentation for several classes.

--- src/cert/CertificateList.h
+++ src/cert/CertificateList.h
@@ -8,29 +8,71 @@ using std::unordered_set;
 
 namespace cert {
 
+    /**
+     * Class represents a list of certificate objects.
+     */
     class CertificateList {
 
     public:
+
+        /**
+         * Constructor for a certificateList.
+         */
         CertificateList();
 
+        /**
+         * Structure to compare certificates by the thumbprint.
+         */
         struct CertificateEqualByThumbprint {
         public:
+
+            /**
+             * Compares two certificates by there thumbprint.
+             * @param lhs First certificate to compare.
+             * @param rhs Second certificate to compare.
+             * @return Boolean if the certificates are equal.
+             */
             bool operator()(const Certificate *lhs, const Certificate *rhs) const;
         };
 
+        /**
+         * Structure to create a hash by the thumbprint.
+         */
         struct CertificateHashByThumbprint {
         public:
+
+            /**
+             * Compares two certificates by their hashes.
+             * @param cert Certificate to compare with.
+             * @return Boolean if the certificates are equal.
+             */
             size_t operator()(const Certificate *cert) const;
         };
 
+        /**
+         * List all certificates.
+         * @return unordered set containing all certificates.
+         */
         unordered_set<Certificate *, CertificateList::CertificateHashByThumbprint, CertificateList::CertificateEqualByThumbprint> *
         listAll();
 
-        void add(Certificate *);
+        /**
+         * Adds a certificate to the certificateList.
+         * @param cert Certificate to add.
+         */
+        void add(Certificate *cert);
 
-        void remove(Certificate *);
+        /**
+         * Removes a certificate from the certificateList.
+         * @param cert Certificate to remove.
+         */
+        void remove(Certificate *cert);
 
     private:
+
+        /**
+         * unordered set of all certificates inside the certificateList.
+         */
         unordered_set<Certificate *, CertificateHashByThumbprint, CertificateEqualByThumbprint> *certificates;
     };
 
--- src/cert/CertificateManager.h
+++ src/cert/CertificateManager.h
@@ -21,58 +21,180 @@ namespace cert {
         KEYPAIR_EXPORT keyPairExport;
     } CERT_EXPORT;
 
+    /**
+     * Class represents a manager to handle logic for certificates.
+     */
     class CertificateManager {
     Q_DECLARE_TR_FUNCTIONS(CertificateManager)
 
     public:
+
+        /**
+         * Constructor for a certificateManager.
+         */
         CertificateManager();
 
-        bool removeCertifcate(Certificate *cert);
+        /**
+         * Removes a certificate from the application.
+         * @param cert Certificate to remove.
+         * @return Boolean if successful or not
+         */
+        bool removeCertificate(Certificate *cert);
 
+        /**
+         * Import a certificate into the application.
+         * @param pathCert Path of the certificate to be imported.
+         * @param pathPrivateKey Path of the key of the certificate to be imported.
+         */
         void importCertificate(string pathCert, string pathPrivateKey);
 
+        /**
+         * Export a certificate to a location.
+         * @param cert Certificate to export.
+         * @param path Location to export to.
+         */
         void exportCertificate(Certificate *cert, string path);
 
+        /**
+         * Export a private key to a location.
+         * @param origin Original location of the private key.
+         * @param destination Target location of the private key.
+         */
         void exportPrivateKeyFile(string origin, string destination);
 
+        /**
+         * Export a private key to a location.
+         * @param pkey Private key to export.
+         * @param location Location to export to.
+         */
         void exportPrivateKey(EVP_PKEY *pkey, string location);
 
+        /**
+         * Get all current certificates.
+         * @return CertificateList containing all current certificates.
+         */
         CertificateList *getCertificateList();
 
+        /**
+         * Loads all certificates from the certman directory into the application.
+         */
         void loadCertificates();
 
+        /**
+         * Loads an X509 object from a path.
+         * @param path Path to load the X509 from.
+         * @return Loaded X509 object.
+         */
         X509 *getX509(string path);
 
+        /**
+         * Loads a private key from a path.
+         * @param path Path to load the private key from.
+         * @return Loaded EVP_KEY.
+         */
         EVP_PKEY *getKey(string path);
 
+        /**
+         * Checks if a certificate has a private key.
+         * @param cert Certificate to check.
+         * @return Boolean if certificate has a private key.
+         */
         bool hasPrivateKey(Certificate *cert);
 
+        /**
+         * Get the default location of a given certificate.
+         * @param cert Certificate to get location from.
+         * @return Location of the certificate.
+         */
         string getCertificateDefaultLocation(Certificate *cert);
 
+        /**
+         * Get the default location of a key from a certificate.
+         * @param cert Certificate to get key location from.
+         * @return Location of the key.
+         */
         string getPrivateKeyDefaultLocation(Certificate *cert);
 
+        /**
+         * Creates a new certificate.
+         * @param subject Subject of the certificate.
+         * @param issuer Issuer of the certificate.
+         * @param validityDays Validity in days of the certificate.
+         * @param keyPair KeyPair of the certificate.
+         * @param signMd Signature message digest of the certificate.
+         * @param serialNumber SerialNumber of the certificate.
+         * @return The created certificate.
+         */
         Certificate *createCertificate(X509_NAME *subject, X509_NAME *issuer, int validityDays, EVP_PKEY *keyPair,
                                        const EVP_MD *signMd, long serialNumber = 1);
 
+        /**
+         * Creates a new certificate.
+         * @param subject Subject of the certificate.
+         * @param issuer Issuer of the certificate.
+         * @param validityDays Validity in days of the certificate.
+         * @param keyPair KeyPair of the certificate.
+         * @param serialNumber SerialNumber of the certificate.
+         * @return The created certificate.
+         */
         Certificate *createCertificate(X509_NAME *subject, X509_NAME *issuer, int validityDays, EVP_PKEY *keyPair,
                                        long serialNumber = 1);
 
+        /**
+         * Gets all certificates inside a X509_STORE object.
+         * @return X509_STORE object containing all certificats.
+         */
         X509_STORE *getCertificateListAsX509Store();
 
+        /**
+         * Signs a given certificate.
+         * @param cert Certificate to sign.
+         * @param pKey Private key used to sign the certificate.
+         * @param parentSubject Subject of the parent certificate.
+         * @return Boolean if signing was successful.
+         */
         bool signCertificate(Certificate *cert, EVP_PKEY *pKey, X509_NAME *parentSubject);
 
+        /**
+         * Genertes a key pair.
+         * @param algorithm Algorithm used.
+         * @param keySize Keysize used.
+         * @return Created private key.
+         */
         EVP_PKEY *generateKeyPair(int algorithm, int keySize);
 
+        /**
+         * Generates keyUsageExtensions.
+         * @param critical Is critical.
+         * @param digitalSignature Allow digitalSignature.
+         * @param nonRepudiation Allow nonRepudiation.
+         * @param keyEncipherment Allow keyEncipherment.
+         * @param dataEncipherment Allow dataencipherment,
+         * @param keyAgreement Allow keyAgreement.
+         * @param keyCertSign Allow keyCertSigning.
+         * @param cRLSign Allow CRLSigning.
+         * @param encipherOnly Allow encipherOnly.
+         * @param decipherOnly Allow decipherOnly.
+         * @return X509_EXTENSION containing all extensions.
+         */
         X509_EXTENSION *
-        generateKeyUsageExtensions(bool critical, bool digitalSignature = false, bool nonRepudation = false,
+        generateKeyUsageExtensions(bool critical, bool digitalSignature = false, bool nonRepudiation = false,
                                    bool keyEncipherment = false,
                                    bool dataEncipherment = false, bool keyAgreement = false, bool keyCertSign = false,
                                    bool cRLSign = false, bool encipherOnly = false,
                                    bool decipherOnly = false);
 
+        /**
+         * Imports a new certificate into the application.
+         * @param newCertificate Certificate to import.
+         */
         void importNewCertificate(CERT_EXPORT newCertificate);
 
     private:
+
+        /**
+         * CertificateList of all certificates.
+         */
         CertificateList *certificateList;
     };
 
