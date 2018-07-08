1c3c9e7c87241f8392357ea82dcc0eaac266972a
Timo R <timo@ribbers.me>
2018-07-08 21:39:28 +0200

    Add main documentation.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,12 @@
 
 using core::Application;
 
+/**
+ * Point of entry.
+ * @param argc Number of arguments.
+ * @param argv Vector of arguments.
+ * @return Exit code.
+ */
 int main(int argc, char *argv[]) {
     auto *app = new Application();
     app->run(argc, argv);
