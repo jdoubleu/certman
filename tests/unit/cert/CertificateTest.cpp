dc8b9ac4d5fb307e0a57036e4dac14ed1171aca7
Timo R <timo@ribbers.me>
2018-07-08 20:22:21 +0200

    Implement tests for certificate getters

--- tests/unit/cert/CertificateTest.cpp
+++ tests/unit/cert/CertificateTest.cpp
@@ -19,7 +19,31 @@ namespace cert {
 
     };
 
-    TEST_F(CertificateTest, NotNull) {
-        EXPECT_NE(certificate, nullptr);
+    TEST_F(CertificateTest, X509NotNull) {
+        EXPECT_NE(certificate->getX509(), nullptr);
+    }
+
+    TEST_F(CertificateTest, VersionEquals) {
+        EXPECT_EQ(certificate->getVersion(), 2);
+    }
+
+    TEST_F(CertificateTest, ThumbprintEquals) {
+        EXPECT_EQ(certificate->getThumbprint(), "04E8CC4D1CC396CDD3E609EEF9961D8044FCE35D");
+    }
+
+    TEST_F(CertificateTest, IssuerEquals) {
+        EXPECT_EQ(certificate->getIssuer(), "CN=*.test.com, O=dollor, OU=sit, C=US, L=ipsum, ST=lorem");
+    }
+
+    TEST_F(CertificateTest, SubjectEquals) {
+        EXPECT_EQ(certificate->getSubject(), "CN=*.test.com, O=dollor, OU=sit, C=US, L=ipsum, ST=lorem");
+    }
+
+    TEST_F(CertificateTest, KeyTypeEquals) {
+        EXPECT_EQ(certificate->getKeyType(), "rsa");
+    }
+
+    TEST_F(CertificateTest, KeySizeEquals) {
+        EXPECT_EQ(certificate->getKeySize(), 256);
     }
 }
